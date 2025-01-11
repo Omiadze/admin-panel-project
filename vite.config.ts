68332ef9bb816f685a4c84617dbc8b6f6a923688
Omiadze <teona.omiadze.1@iliauni.edu.ge>
2025-01-11 20:04:03 +0400

    trying to fix deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/en/", // Default for Vercel
+  base: "/", // Default for Vercel
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
